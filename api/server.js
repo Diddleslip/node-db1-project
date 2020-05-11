24189ae3d1f2b3088c26f3f2804d81106eef6da5
Diddleslip <diddleslip@gmail.com>
2020-05-11 17:40:16 -0400

    Finished MVP + Stretch Questions

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const accountsRouter = require("../server-routers/accounts-router");
 
 const server = express();
 
-server.use(express.json());
+server.use(express.json()); // Built in middleware, no need to install it
+
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "Good to go!" });
+})
+  
+server.use("/api/accounts", accountsRouter);
 
 module.exports = server;
